ac3738a6a0f8a4726ae0f5e411d8356f3c5ffbd6
vlauncher <v2dent@gmail.com>
2020-09-14 14:02:48 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # django-project
+
+ok ill try to create an authentication system now
--- g5techpoultry/settings.py
+++ g5techpoultry/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
+AUTH_USER_MODEL = 'users.CustomUser' # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
